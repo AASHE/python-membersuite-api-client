d26459b070ffb0770ae48f9b050dab9ea2e1fe0a
Benjamin Stookey <ben.stookey@gmail.com>
2017-01-26 16:54:20 -0700

    Updates setup with version and requirements;

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='membersuite-api-client',
-      version='0.1.1',
+      version='0.1.2',
       description='MemberSuite API Client',
       author='AASHE',
       author_email='webdev@aashe.org',
@@ -25,4 +25,5 @@ setup(name='membersuite-api-client',
           'Programming Language :: Python :: 3.4.3',
       ],
       include_package_data=True,
+      install_requires=["zeep>=0.26"]
       )
