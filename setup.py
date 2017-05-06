bba9fe86fb9d88d877f361c1c4cc8cbee855546a
Robert Erb <bob.erb@gmail.com>
2017-05-06 18:50:26 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 
 setup(name='membersuite_api_client',
-      version="0.3.3",
+      version="0.4",
       description='MemberSuite API Client',
       author='AASHE',
       author_email='webdev@aashe.org',
